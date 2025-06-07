c953e9e7a421936dd1127379c05474adee89cae4
MonoZikk <monozikk13@gmail.com>
2025-06-07 16:42:26 +0700

    Membuat authentication login resepsionis

---
+++ HomeStay/DBConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeStay
+{
+    public static class DBConfig
+    {
+        public static string dbServer = "localhost";
+        public static string dbUser = "root";
+        public static string dbName = "homestay";
+        public static string dbPass = "";
+
+        public static string ConnStr = $"server={dbServer}; user={dbUser}; database={dbName}; password={dbPass}";
+    }
+}
--- HomeStay/FormLogin.cs
+++ HomeStay/FormLogin.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto.Generators;
 
 namespace HomeStay
 {
@@ -27,7 +29,60 @@ namespace HomeStay
 
         }
 
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text.Trim();
+            string password = textBoxPassword.Text.Trim();
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Username dan Password harus diisi!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using(MySqlConnection conn = new MySqlConnection(DBConfig.ConnStr))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "Select password from resepsionis where username = @username";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@username", username);
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        string hashFromDb = reader.GetString("password");
+                        bool isValid = BCrypt.Net.BCrypt.Verify(password, hashFromDb);
+                        if (isValid)
+                        {
+                            // Login sukses 
+                            MessageBox.Show("Login berhasil!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            FormReservasi frm = new FormReservasi();
+                            this.Hide();
+                            frm.Show();
+                        }
+                        else
+                        {
+                            // Password salah
+                            MessageBox.Show("Password salah!", "Login Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        // Username tidak ditemukan
+                        MessageBox.Show("Username tidak ditemukan!", "Login Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Koneksi ke database gagal: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
 
 
+            }
+        }
     }
 }
