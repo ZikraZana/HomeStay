400002bf2f26ba0489f136c249dd2eeda3d17736
MonoZikk <monozikk13@gmail.com>
2025-06-07 17:03:47 +0700

    Memperbaiki textbox password agar ketika user mengisi password maka hasil typingnya disensor (*)

--- HomeStay/FormLogin.Designer.cs
+++ HomeStay/FormLogin.Designer.cs
@@ -70,6 +70,7 @@
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.Size = new System.Drawing.Size(194, 22);
             this.textBoxPassword.TabIndex = 5;
+            this.textBoxPassword.UseSystemPasswordChar = true;
             // 
             // FormLogin
             // 
